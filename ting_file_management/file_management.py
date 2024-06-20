e01e565d678af8184b4a31e8fa66197b7d1eb142
Gabriel Amorim <ricksamann@gmail.com>
2024-06-19 21:34:39 -0300

    feat: req02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.split(".")[1] != "txt":
+        return sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError as e:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")

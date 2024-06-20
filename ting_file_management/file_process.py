56f03618a958d23c2f88a22cadd75e6f2c111a08
Gabriel Amorim <ricksamann@gmail.com>
2024-06-19 23:10:28 -0300

    feat: req03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    exist = False
+    for inst in instance.data:
+        if inst["nome_do_arquivo"] == path_file:
+            exist = True
+
+    if not exist:
+        data = txt_importer(path_file)
+        dataFormated = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(data),
+            "linhas_do_arquivo": data,
+        }
+        instance.enqueue(dataFormated)
+        return print(dataFormated)
 
 
 def remove(instance):

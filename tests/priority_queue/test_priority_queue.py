4005a2cf24fa961415583f65a1eb71db0a3d1006
Gabriel Amorim <ricksamann@gmail.com>
2024-06-20 00:42:48 -0300

    feat: req06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+    pq.enqueue({"qtd_linhas": 2})
+    pq.enqueue({"qtd_linhas": 10})
+    pq.enqueue({"qtd_linhas": 5})
+    pq.enqueue({"qtd_linhas": 4})
+
+    assert len(pq) == 4
+
+    assert pq.search(0) == {"qtd_linhas": 2}
+    assert pq.search(1) == {"qtd_linhas": 4}
+    assert pq.search(2) == {"qtd_linhas": 10}
+    assert pq.search(3) == {"qtd_linhas": 5}
+
+    with pytest.raises(IndexError):
+        pq.search(4)
+
+    assert pq.dequeue() == {"qtd_linhas": 2}
+    assert pq.dequeue() == {"qtd_linhas": 4}
+    assert len(pq) == 2

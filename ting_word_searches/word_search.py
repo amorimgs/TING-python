8c66873203c02979e2e3eedfe934167a466cdde0
Gabriel Amorim <ricksamann@gmail.com>
2024-06-20 16:11:10 -0300

    feat: req07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in instance.data:
+        ocorrencias = []
+        for index in range(len(i["linhas_do_arquivo"])):
+            if word.lower() in i["linhas_do_arquivo"][index].lower():
+                ocorrencias.append({"linha": index + 1})
+        if ocorrencias:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": i["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
